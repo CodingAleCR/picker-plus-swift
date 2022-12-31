7141e3e1204ca1a6956747a3279b679c84e1f42a
Alejandro Ulate <me@codingale.dev>
2022-12-30 20:07:47 -0600

    Initial Commit

---
+++ UISegmentControl/UISegmentControlApp.swift
@@ -0,0 +1,17 @@
+//
+//  UISegmentControlApp.swift
+//  UISegmentControl
+//
+//  Created by Alejandro Ulate on 30/12/22.
+//
+
+import SwiftUI
+
+@main
+struct UISegmentControlApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
