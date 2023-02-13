883185c8ad71f3de2be46c5b2e2ea38c99089ab4
Alejandro Ulate <me@codingale.dev>
2023-02-13 05:03:14 -0600

    feat: adding custom indicators

--- PickerPlus/UI/PickerPlus.swift
+++ PickerPlus/UI/PickerPlus.swift
@@ -37,7 +37,19 @@ public struct PickerPlus<Data, Content> : View where Data: Hashable, Content: Vi
         return view
     }
     
-    // TODO: Add support for custom indicators
+    private var customIndicator: AnyView? = nil
+    
+    public init(
+        _ sources: [Data],
+        selection: Data?,
+        indicatorBuilder: @escaping () -> some View,
+        @ViewBuilder itemBuilder: @escaping (Data) -> Content
+    ) {
+        self.sources = sources
+        self.selection = selection
+        self.itemBuilder = itemBuilder
+        self.customIndicator = AnyView(indicatorBuilder())
+    }
     
     public init(
         _ sources: [Data],
@@ -51,7 +63,21 @@ public struct PickerPlus<Data, Content> : View where Data: Hashable, Content: Vi
     
     public var body: some View {
         ZStack(alignment: .center) {
-            // TODO: Add indicator
+            if let selection = selection, let selectedIdx = sources.firstIndex(of: selection) {
+                if let customIndicator = customIndicator {
+                    customIndicator
+                } else {
+                    GeometryReader { geo in
+                        RoundedRectangle(cornerRadius: cornerRadius ?? 6.0)
+                            .foregroundColor(.accentColor)
+                            .padding(EdgeInsets(top: borderWidth ?? 2, leading: borderWidth ?? 2, bottom: borderWidth ?? 2, trailing: borderWidth ?? 2))
+                            .frame(width: geo.size.width / CGFloat(sources.count))
+                            .shadow(color: .black.opacity(0.1), radius: 2, x: 1, y: 1)
+                            .animation(.spring().speed(1.5))
+                            .offset(x: geo.size.width / CGFloat(sources.count) * CGFloat(selectedIdx), y: 0)
+                    }.frame(height: 32)
+                }
+            }
             
             HStack(spacing: 0) {
                 ForEach(sources, id: \.self) { item in
@@ -94,9 +120,41 @@ struct PreviewPickerPlus: View {
                     }
             }
             .pickerBackgroundColor(.blue.opacity(0.15))
-            .borderWidth(2)
+            .cornerRadius(0)
+            .borderWidth(1)
             .borderColor(.blue)
-            .accentColor(.green)
+            .accentColor(.blue)
+            .padding()
+            
+            Text("Custom Picker: Vertical Items")
+            PickerPlus(
+                Vehicles.allCases,
+                selection: selectedItem,
+                indicatorBuilder: {
+                    GeometryReader { geo in
+                        Rectangle()
+                            .foregroundColor(.white)
+                            .cornerRadius(6.0)
+                            .padding(1)
+                            .frame(width: geo.size.width / CGFloat(Vehicles.allCases.count))
+                            .shadow(color: .black.opacity(0.1), radius: 2, x: 1, y: 1)
+                            .animation(.spring().speed(1.5))
+                            .offset(x: geo.size.width / CGFloat(Vehicles.allCases.count) * CGFloat(Vehicles.allCases.firstIndex(of: selectedItem!)!), y: 0)
+                    }.frame(height: 64)
+                }
+            ) { item in
+                VerticalItem(
+                    item: item,
+                    isSelected: selectedItem == item
+                )
+                .padding(.vertical, 8)
+                
+                .onTapGesture {
+                    withAnimation(.easeInOut(duration: 0.150)) {
+                        selectedItem = item
+                    }
+                }
+            }
             .padding()
         }
     }
